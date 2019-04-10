68d1dae24926e848a73313d335f3703b75b5f701
Euan Medway <euanmedway@yahoo.co.uk>
2019-04-11 00:52:27 +0100

    Migrated to discord.py version 1.0.1.

--- hangout.py
+++ hangout.py
@@ -11,23 +11,24 @@ client = discord.Client()
 @client.event 
 async def on_ready():
     print("Online and Ready to Hang!")
-    await client.change_presence(game=discord.Game(name="Hangout | >hangout"))
+    await client.change_presence(activity = discord.Game("Hangout | >hangout"))
 
 @client.event
 async def on_message(message):
     if message.content == ">hangout":
-        if message.author.voice.voice_channel != None:
-            voice_channel = message.author.voice.voice_channel.id
-            url = "https://discordapp.com/channels/" + str(message.server.id) + "/" + str(voice_channel)
-            members = message.author.voice.voice_channel.voice_members
+        if message.author.voice != None:
+            voice_channel = message.author.voice.channel.id
+            url = "https://discordapp.com/channels/" + str(message.guild.id) + "/" + str(voice_channel)
+            members = message.author.voice.channel.members
             invite = discord.Embed(description = "You have been invited to a Hangout: \n" + str(url), color = 0x59b1ff)
             for member in members:
-                await client.send_message(member, embed = invite)
-
+                if member.dm_channel == None:
+                   await member.create_dm()
+                await member.dm_channel.send(embed = invite)
             complete = discord.Embed(description = "You're invite has been sent out!", color = 0x5adb51)
-            await client.send_message(message.channel, embed = complete)
+            await message.channel.send(embed = complete)
         else:
             failed = discord.Embed(description = "You are not in a voice channel!", color = 0xffa13d)
-            await client.send_message(message.channel, embed = failed)
+            await message.channel.send(embed = failed)
 
 client.run(token)
