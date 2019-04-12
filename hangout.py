03312d639d76098da4305d53e33bbb6ba0996933
Euan Medway <euanmedway@yahoo.co.uk>
2019-04-12 04:08:44 +0100

    Added support for custom prefixes.

--- hangout.py
+++ hangout.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import os
 from discord.ext.commands import Bot
 from discord.ext import commands
 from discord.utils import get
@@ -15,7 +16,39 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content == ">hangout":
+    if message.author == client.user:
+        return False
+    server = message.guild
+    os.chdir(os.path.dirname(__file__))
+    location = os.getcwd()+"/servers/"
+    if not os.path.exists(location):
+        os.makedirs(location)
+    if not os.path.exists(location+str(server.id)):
+        os.makedirs(location+str(server.id))
+
+    file_name = location+str(server.id)+r"\prefix.txt"
+    file = open(file_name, "a+")
+    file.close
+    file = open(file_name, "r")
+    for line in file.readlines():
+        for x in line:
+            if x == "":
+                file.close
+                file = open(file_name, "w")
+                file.write(">")
+                file.close
+                file = open(file_name, "r")
+                print(file.read())
+                file.close
+            else:
+                file.close
+
+    file = open(file_name, "r")
+    line = file.readline()
+    prefix = line
+    file.close
+    
+    if message.content == prefix+"hangout":
         if message.author.voice != None:
             voice_channel = message.author.voice.channel.id
             url = "https://discordapp.com/channels/" + str(message.guild.id) + "/" + str(voice_channel)
@@ -30,5 +63,25 @@ async def on_message(message):
         else:
             failed = discord.Embed(description = "You are not in a voice channel!", color = 0xffa13d)
             await message.channel.send(embed = failed)
+
+    elif message.content == prefix+"prefix":
+        if not message.channel.permissions_for(message.author).manage_messages:
+            prefix_permissions_failed = discord.Embed(description = "You do not have the correct permissions to change the prefix", color = 0xffa13d)
+            await message.channel.send(embed = prefix_permissions_failed)
+        else:
+            prefix_change = discord.Embed(description = "What would you like to change the prefix to?", color = 0x59b1ff)
+            await message.channel.send(embed = prefix_change)
+
+            def check(m):
+                return m.author == message.author and m.channel == message.channel          
+
+            reply = await client.wait_for('message', check = check)
+            file = open(file_name, "w")
+            file.write(reply.content)
+            file.close
+            prefix = reply.content
+            prefix_success = discord.Embed(description = "Prefix changed to "+prefix, color = 0x5adb51)
+            await message.channel.send(embed = prefix_success)
+ 
 
 client.run(token)
